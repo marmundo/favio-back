3e174427c016d8b3fb1c51d66a9c97b8d1581218
Marcelo Damasceno <marcelodamasceno@gmail.com>
2023-12-12 14:40:58 -0300

    adiciona controller para bookmark

--- app/Controllers/Http/BookmarksController.ts
+++ app/Controllers/Http/BookmarksController.ts
@@ -1,15 +1,48 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+const favoritos = [{ id: 1, nome: 'Google', url: 'http://www.google.com', importante: true }]
+
 export default class BookmarksController {
   public async index({}: HttpContextContract) {
-    return [{ id: 0, nome: 'bookmark', url: 'bookmark.com', importante: true }]
+    return favoritos
   }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request, response }: HttpContextContract) {
+    const { nome, url, importante } = request.body()
+    const newFavorito = { id: favoritos.length, nome, url, importante }
+    favoritos.push(newFavorito)
+    return response.status(201).send(newFavorito)
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ params }: HttpContextContract) {
+    // eslint-disable-next-line eqeqeq
+    let favoritoEncontrado = favoritos.find((favorito) => favorito.id == params.id)
+    if (favoritoEncontrado === undefined) return { msg: 'favorito nao encontrado' }
+    return favoritoEncontrado
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ params, request, response }: HttpContextContract) {
+    const { nome, url, importante } = request.body()
+    // eslint-disable-next-line eqeqeq
+    let favoritoEncontrado = favoritos.find((favorito) => favorito.id == params.id)
+    if (!favoritoEncontrado) {
+      return response.status(404).json({ error: 'Favorite not found' })
+    }
 
-  public async destroy({}: HttpContextContract) {}
+    const index = favoritos.indexOf(favoritoEncontrado)
+    favoritos[index] = { id: index, nome, url, importante }
+    return response.status(201).json({ id: index, nome, url, importante })
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    // eslint-disable-next-line eqeqeq
+    let favoritoEncontrado = favoritos.find((favorito) => favorito.id == params.id)
+    if (!favoritoEncontrado) {
+      return response.status(404).json({ error: 'Favorite not found' })
+    }
+
+    const index = favoritos.indexOf(favoritoEncontrado)
+    favoritos.splice(index, 1)
+    return response.status(204)
+  }
 }
